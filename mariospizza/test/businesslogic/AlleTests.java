daed11a26c9e0bcbad1ef0673e966bc131e3b4bc
wosch <wosch@10.50.130.216>
2019-04-11 15:14:26 +0200

    Aske: Tilføjede mere til mockStorage. Malthe: Tilføjede mere til AlleTests.

--- mariospizza/test/businesslogic/AlleTests.java
+++ mariospizza/test/businesslogic/AlleTests.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import presentation.FakeUI;
 import storage.mockStorage;
 
 /**
@@ -15,7 +16,11 @@ import storage.mockStorage;
 public class AlleTests {
     @Test
     public void testStart() {
-        
+        String[] input = {"9"}; 
+        FakeUI ui = new FakeUI(input);
+        Controller ctrl = new Controller(ui, new mockStorage(), new FileWriterHistorik());
+        ctrl.start();
+        assertTrue(ui.output.get(0).contains("muligheder"));
     }
     
     @Test
@@ -26,24 +31,34 @@ public class AlleTests {
     }
     
     @Test
-     public void opretBestillingEnPizza() {
-         
-     }
-    
+     public void opretBestillingEnPizza()  {
+        mockStorage storage = new mockStorage();
+        int[] pizza = {1};
+        storage.addToOrdre(new Bestilling(pizza,0,"20:20","allan","123"));
+        
+        assertTrue(storage.getBestillinger().size()==1);
+    }
     @Test
     public void testVisBestillinger() {
+        mockStorage storage = new mockStorage();
+        int[] pizza = {1};
+        storage.addToOrdre(new Bestilling(pizza,0,"20:20","allan","123"));
         
+        assertTrue(storage.getBestillinger().get(0).getAfhentningsTidspunkt().equals("20:20"));
     }
     
     @Test
     public void testFjernBestilling() {
+        mockStorage storage = new mockStorage();
+        int[] pizza = {1};
+        storage.addToOrdre(new Bestilling(pizza,0,"20:20","allan","123"));
         
+        assertTrue(storage.getBestillinger().size()==1);
+        
+        storage.fjernBestilling(1);
+        
+        assertTrue(storage.getBestillinger().size()==0);
+
     }
-    
-    @Test
-     public void testHistorik() {
-         
-    }
-    
     
 }
